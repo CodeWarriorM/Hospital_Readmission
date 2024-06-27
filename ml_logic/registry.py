d7531dc857a4588842803da203a53ecf839f1d67
Olaf Hilgenfeld <olaf.hilgenfeld@gmail.com>
2024-06-27 12:14:17 +0200

    adapted frontend (frontend_olaf.py) for prediction

--- ml_logic/registry.py
+++ ml_logic/registry.py
@@ -2,14 +2,22 @@ import os
 import glob
 import pickle
 
-def load_model():
+def load_model(preferred=None):
     project_path = os.path.dirname(os.path.dirname(__file__))
     list_of_models = glob.glob(project_path + '/models/*.pkl')
     latest_model = max(list_of_models, key=os.path.getctime)
 
-    with open(latest_model, 'rb') as file:
+    if preferred is None:
+        model_to_open = latest_model
+    elif (project_path + f'/models/{preferred}') not in list_of_models:
+        model_to_open = latest_model
+        print('Preferred model not found. Will load latest model instead...')
+    else:
+        model_to_open = project_path + f'/models/{preferred}'
+
+    with open(model_to_open, 'rb') as file:
         model = pickle.load(file)
 
-    print('loaded model: ', {latest_model.split('/')[-1]})
+    print('loaded model: ', {model_to_open.split('/')[-1]})
 
     return model
