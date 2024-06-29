642f7d335fdfb1151c4fdbc9e77b000f61cf4d7c
Manuela Brunner <manuela@brunner-sr.de>
2024-06-29 17:43:47 +0200

    changed brace and json response like Vlad said

--- packages/frontend_olaf.py
+++ packages/frontend_olaf.py
@@ -9,41 +9,14 @@ import numpy as np
 from ml_logic.data import map_age, map_n_diverse
 
 #st.image('raw_data/hospital.jpg', caption='Hospital', use_column_width=True)
-def convert_jpg_to_png(jpg_file, png_file):
-    with Image.open(jpg_file) as img:
-        img.save(png_file, 'PNG')
 
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data=f.read()
-    return base64.b64encode(data).decode()
-
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = f'''
-    <style>
-    body {{
-    background-image: url("data:image/png;base64,{bin_str}");
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    }}
-    </style>
-    '''
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-
-jpg_file = 'raw_data/hospital.jpg'
-png_file = 'raw_data/hospital.png'
-convert_jpg_to_png(jpg_file, png_file)
-
-set_background(png_file)
 
 st.title('Hospital Readmission App')
 st.header('Will my patient be readmitted to the hospital within 30 days after discharge?')
 
 st.write('Select your features below:')
 
-age = st.selectbox('Age', ['[0-10)', '[10-20)', '[20-30)', '[30-40)', '[40-50)', '[50-60)', '[60-70)', '[70-80)', '[80-90)', '[90-100)'])
+age = st.selectbox('Age', ['[0-10]', '[10-20)', '[20-30)', '[30-40)', '[40-50)', '[50-60)', '[60-70)', '[70-80)', '[80-90)', '[90-100)'])
 diag_1 = st.selectbox('Primary Diagnosis', ['Circulatory',  'Injury', 'Digestive', 'Respiratory','Diabetes', 'Musculoskeletal', 'Other','Missing'])
 diag_2 = st.selectbox('Secondary Diagnosis 1', ['Circulatory',  'Injury', 'Digestive', 'Respiratory','Diabetes', 'Musculoskeletal', 'Other','Missing'])
 diag_3 = st.selectbox('Secondary Diagnosis 2', ['Circulatory',  'Injury', 'Digestive', 'Respiratory', 'Diabetes', 'Musculoskeletal','Other', 'Missing'])
@@ -84,39 +57,42 @@ if st.button('Predict'):
     })
 
 # Method 1
+#url = 'http://127.0.0.1:8000/predict'
 url = 'https://hospitalreadmission1575-jgoxnpqt5a-ew.a.run.app/predict'
 params = {'time_in_hospital': time_in_hospital,
-          'n_lab_procedures': n_lab_procedures,
-          'n_procedures': n_procedures,
-          'n_medications': n_medications,
-          'n_outpatient': n_outpatient,
-          'n_inpatient': n_inpatient,
-          'n_emergency': n_emergency,
-          'age' : age,
-          'diag_1' : diag_1,
-          'diag_2' : diag_2,
-          'diag_3' : diag_3,
-          'A1Ctest' : A1Ctest,
-          'change' : change,
-          'diabetes_med' : diabetes_med
-          }
+            'n_lab_procedures': n_lab_procedures,
+            'n_procedures': n_procedures,
+            'n_medications': n_medications,
+            'n_outpatient': n_outpatient,
+            'n_inpatient': n_inpatient,
+            'n_emergency': n_emergency,
+            'age' : age,
+            'diag_1' : diag_1,
+            'diag_2' : diag_2,
+            'diag_3' : diag_3,
+            'A1Ctest' : A1Ctest,
+            'change' : change,
+            'diabetes_med' : diabetes_med
+            }
 
 
 response = requests.get(url=url,
-                        params=params).json()
+                        params=params)
+print(response.content)
+response = response.json()
 
 
 
 st.write('Will my patient be readmitted?')
 st.write(str(response['Hospital readmission:'].capitalize()))
 
-def display_shap_summary_plot(shap_values, X):
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    plt.title('SHAP Summary Plot')
-    shap.summary_plot(shap_values, X, plot_type="bar")
-    st.pyplot(bbox_inches='tight')
-    plt.clf()
-st.title("SHAP Summary Plot")
+#def display_shap_summary_plot(shap_values, X):
+    #st.set_option('deprecation.showPyplotGlobalUse', False)
+    #plt.title('SHAP Summary Plot')
+    #shap.summary_plot(shap_values, X, plot_type="bar")
+    #st.pyplot(bbox_inches='tight')
+    #plt.clf()
+#st.title("SHAP Summary Plot")
 
-if st.button('Generate SHAP Summary Plot'):
-    display_shap_summary_plot(shap.shap_values, X_pred)
+#if st.button('Generate SHAP Summary Plot'):
+    #display_shap_summary_plot(shap.shap_values, X_pred)
